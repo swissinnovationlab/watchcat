6fd7262b6624f3670e8819410649cc63dbe5d4b4
schef <zvanstefan@gmail.com>
2024-11-27 11:49:42 +0100

    added dryrun to

--- scripts/watchcat.sh
+++ scripts/watchcat.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-REPORT_FILE="~/watchcat/logs/watchcat_reboot_reason.txt"
+SCRIPT_DIR=$(dirname "$0")
+REPORT_FILE="$SCRIPT_DIR"/../logs/watchcat_reboot_reason.txt
 CURRENT_TIME=$(date --utc +%s)
 
 FILES=(
   "/tmp/watchcat_openvpn_dmp.txt"
   "/tmp/watchcat_devconn_connector.txt"
 )
+
+DRYRUN=0
 
 is_timestamp_old() {
   local timestamp="$1"
@@ -39,23 +42,48 @@ check_file() {
   local file="$1"
   if [ -f "$file" ]; then
     TIMESTAMP=$(cat "$file" | grep -oE '[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}(\.[0-9]+)?(Z|\+00:00)?')
+    if [ -z "$TIMESTAMP" ]; then
+        echo "TIMESTAMP could not be parsed"
+        TIMESTAMP=NONPARSE
+    else
+        echo "TIMESTAMP is $TIMESTAMP"
+    fi
+
     if is_timestamp_old "$TIMESTAMP"; then
-      echo "Reboot triggered by $file with timestamp $TIMESTAMP; current UTC time: $(date --utc +%Y-%m-%dT%H:%M:%S%z)" > "$REPORT_FILE"
+      echo "Reboot triggered by $file with timestamp $TIMESTAMP; current UTC time: $(date --utc +%Y-%m-%dT%H:%M:%S%z) DRYRUN=$DRYRUN" >> "$REPORT_FILE"
       echo "Rebooting system due to file $file with old timestamp."
-      #sudo reboot
+      [ $DRYRUN -eq 0 ] && sudo reboot || echo "Reboot skipped due to dry run flag"
     else
       REMAINING_TIME=$(time_until_timeout "$TIMESTAMP")
       echo "$file $REMAINING_TIME till timeout"
     fi
+  else
+    echo "$file does not exist"
   fi
 }
 
-UPTIME_HOURS=$(awk '{print int($1/3600)}' /proc/uptime)
-if [ "$UPTIME_HOURS" -ge 2 ]; then
-  for FILE in "${FILES[@]}"; do
-    check_file "$FILE"
-  done
-else
-  echo "System uptime is less than 2 hours. No need to check files."
-fi
+echo "Starting script"
 
+check_args() {
+  for arg in "$@"
+  do
+      if [ "$arg" == "--dryrun" ]; then
+          DRYRUN=1
+          echo "--dryrun FLAG is called"
+      fi
+  done
+}
+
+main() {
+  UPTIME_HOURS=$(awk '{print int($1/3600)}' /proc/uptime)
+  if [ "$UPTIME_HOURS" -ge 2 ]; then
+    for FILE in "${FILES[@]}"; do
+      check_file "$FILE"
+    done
+  else
+    echo "System uptime is less than 2 hours. No need to check files."
+  fi
+}
+
+check_args "$@"
+main
